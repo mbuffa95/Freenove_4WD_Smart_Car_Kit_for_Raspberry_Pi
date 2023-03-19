cdea389b54d0bcdce4499bdbe2e12f6b6a815e52
Matthew <mbuffa@oakland.edu>
2023-03-19 14:42:39 -0400

    averaging slopes and plotting 2 lane lanes

--- Code/Server/new_lane_keep.py
+++ Code/Server/new_lane_keep.py
@@ -4,6 +4,7 @@ import matplotlib.image as mpimg
 import time
 import matplotlib.pyplot as plt
 import os
+import logging
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import servo
@@ -70,12 +71,77 @@ def display_lines(frame, lines, line_color=(0, 255, 0), line_width=10):
     line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
     return line_image
 
+def make_points(frame, line):
+    height, width, _ = frame.shape
+    slope, intercept = line
+    y1 = height  # bottom of the frame
+    y2 = int(y1 * 7 / 10)  # make points from middle of the frame down
+
+    # bound the coordinates within the frame
+    x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
+    x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+    return [[x1, y1, x2, y2]]
+
+def average_slope_intercept(frame, line_segments):
+    """
+    This function combines line segments into one or two lane lines
+    If all line slopes are < 0: then we only have detected left lane
+    If all line slopes are > 0: then we only have detected right lane
+    """
+    lane_lines = []
+    if line_segments is None:
+        logging.info('No line_segment segments detected')
+        return lane_lines
+
+    height, width, _ = frame.shape
+    left_fit = []
+    right_fit = []
+
+    boundary = 1/3
+    left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
+    right_region_boundary = width * boundary # right lane line segment should be on left 2/3 of the screen
+
+    for line_segment in line_segments:
+        for x1, y1, x2, y2 in line_segment:
+            if x1 == x2:
+                logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
+                continue
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0:
+                if x1 < left_region_boundary and x2 < left_region_boundary:
+                    left_fit.append((slope, intercept))
+            else:
+                if x1 > right_region_boundary and x2 > right_region_boundary:
+                    right_fit.append((slope, intercept))
+
+    left_fit_average = np.average(left_fit, axis=0)
+    if len(left_fit) > 0:
+        lane_lines.append(make_points(frame, left_fit_average))
+
+    right_fit_average = np.average(right_fit, axis=0)
+    if len(right_fit) > 0:
+        lane_lines.append(make_points(frame, right_fit_average))
+
+    logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+
+    return lane_lines
+
+
+
 cv2.imshow("masked edges", masked_edges)
 cropped_img = region_of_interest(masked_edges)
+
 cv2.imshow("cropped image", cropped_img)
+
 line_segs = detect_line_segments(cropped_img)
 line_segs_img = display_lines(image, line_segs)
 cv2.imshow("detected lines", line_segs_img)
+
+lane_lines = average_slope_intercept(image, line_segs)
+lane_lines_img = display_lines(image, lane_lines)
+cv2.imshow("lane lines", lane_lines_img)
 
 '''rho = 1
 theta = np.pi/180
