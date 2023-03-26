8559f823215e3107e7bb3713ccfd20a7af42dd2b
Matthew Buffa <mbuffa@oakland.edu>
2023-03-26 12:04:40 -0400

    testing quit key

--- Code/Server/new_lane_keep.py
+++ Code/Server/new_lane_keep.py
@@ -10,6 +10,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import servo
 import Motor
+import keyboard
 
 def region_of_interest(edges):
    height, width = edges.shape
@@ -263,7 +264,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
             PWM.setMotorModel(1000,1000,1000,1000)
         elif new_stable_steering_angle > 45 and new_stable_steering_angle < 87:
             print('turning left')
-            PWM.setMotorModel(250,250,1000,1000)       #Left 
+            #PWM.setMotorModel(250,250,1000,1000)       #Left 
         elif new_stable_steering_angle > 93 and new_stable_steering_angle < 135:
             print('turning right')
             PWM.setMotorModel(1000,1000,250,250)       #Right 
@@ -287,7 +288,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     frame.truncate(0)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q') or keyboard.is_pressed("q"):
         cleanup()
         break
 
