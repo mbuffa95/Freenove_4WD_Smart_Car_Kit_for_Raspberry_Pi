30ddaf1ed03ae9408eb5b5ba91e312f8a307cebf
Matthew Buffa <mbuffa@oakland.edu>
2023-04-08 08:13:17 -0400

    first attempt at slowing down based on avg dist

--- Code/Server/stop_go_fca.py
+++ Code/Server/stop_go_fca.py
@@ -10,10 +10,24 @@ import servo
 import Motor
 from Ultrasonic import *
 
-target_speed = 1000
+target_speed = 750
 num_readings_to_avg = 5
 
+stop_dist = 5
+slow_dist = 25
+
 ultrasonic=Ultrasonic()
+PWM= Motor.Motor()
+
+# center ultrasonic sensor
+servo = servo.Servo()
+servo.setServoPwm('0',90)
+servo.setServoPwm('1',90)
+
+# start at zero speed
+PWM.setMotorModel(0, 0, 0, 0)
+
+
 
 try:
     while True:
@@ -24,6 +38,14 @@ try:
       
       avg_dist = (dist / num_readings_to_avg)
       print ("Averaged obstacle distance is "+str(avg_dist)+"CM")
+
+      if( avg_dist > slow_dist ):
+        PWM.setMotorModel(target_speed, target_speed, target_speed, target_speed)
+      elif (avg_dist <= slow_dist) and (avg_dist > stop_dist):
+        PWM.setMotorModel(target_speed * 0.5, target_speed * 0.5, target_speed * 0.5, target_speed * 0.5)
+      else:
+        PWM.setMotorModel(0, 0, 0, 0)
+
       time.sleep(1)
       
 except KeyboardInterrupt:
